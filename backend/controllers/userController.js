9e73821bf48a258103840ad117eab9db23546033
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-05-09 19:53:27 +0530

    Auth Protect Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,73 +6,77 @@ import User from "../models/userModel.js";
 // @route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
-        generateToken(res, user._id);
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-        })
-    } else {
-        res.status(401);
-        throw new Error("Invalid email or password");
-    }
-
-
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email });
 
-    if (userExists) {
-        res.status(400);
-        throw new Error("User already exists");
-    }
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    const user = await User.create({
-      name,
-      email,
-      password  
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
     });
-
-    if (user) {
-        generateToken(res, user._id);
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-        })
-    } else {
-        res.status(400);
-        throw new Error("Invalid user data");
-    }
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Logout user
 // @route POST /api/users/logout
 // @access Public
 const logoutUser = (req, res) => {
-    res.cookie('jwt', '', {
-      httpOnly: true,
-      expires: new Date(0),
-    });
-    res.status(200).json({ message: 'User Logged out successfully!' });
-  };
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User Logged out successfully!" });
+};
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User profile" });
+  const user = { 
+    _id: req.user._id, 
+    name: req.user.name, 
+    email: req.user.email 
+                };
+
+  res.status(200).json(user);
 });
 
 // @desc Update user profile
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express  from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
@@ -13,6 +14,7 @@ const app = express();
 // middlewares 
 app.use(express.json()); // Parses incoming requests with JSON payloads.
 app.use(express.urlencoded({ extended: true })); // Parses incoming requests with URL-encoded payloads, including nested objects.
+app.use(cookieParser()); // Parses cookies sent by the client to the server.
 
 // End points
 app.use('/api/users', userRoutes);
