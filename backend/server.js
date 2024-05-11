9c6b413a006313139f03a99c6bd21ef05e865454
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-05-11 18:01:48 +0530

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express  from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -5,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
 
 connectDB();
 const port = process.env.PORT || 5000;
@@ -15,17 +17,25 @@ const app = express();
 app.use(express.json()); // Parses incoming requests with JSON payloads.
 app.use(express.urlencoded({ extended: true })); // Parses incoming requests with URL-encoded payloads, including nested objects.
 app.use(cookieParser()); // Parses cookies sent by the client to the server.
+app.use(cors());
 
 // End points
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('Server is ready!'));
+// Serve static files from the frontend build directory
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-// Error middleware
+// Serve frontend index.html for any route not handled by API
+app.get("*", (req, res) => {
+    res.send(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
-// listen to port
+// Start server
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 })
