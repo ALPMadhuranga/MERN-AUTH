0be0af0e56b71a9260e596b7690528d204dd5dfe
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-05-11 13:11:54 +0530

    Create Auth Slice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,18 +17,18 @@ import RegisterScreen from "./screens/RegisterScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Provider store={store}>
-      <Route path="/" element={<App />}>
-        <Route index={true} path="/" element={<HomeScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-      </Route>
-    </Provider>
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+    </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'   // This function helps us set up a Redux store, which is like a global state for our app.
+import authReducer from './slices/authSlice'  // This is the file where we defined our initial state and reducers.
 
 const store = configureStore({  // This function call initializes a Redux store with the provided configuration options.
-  reducer: {},  // We're saying our initial state (the data in our store) is empty {}.
+  reducer: {    // We're saying our initial state (the data in our store) is empty {}.
+    auth: authReducer,  // We're adding a new key called "auth" and assigning it the value of the authReducer.
+  },  
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // We're using default settings for handling data changes (middleware).
   devTools: true,  // We're turning on a tool called Redux DevTools, which helps us track and debug our data changes.
 });
