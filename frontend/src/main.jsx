6df8e7f69bf1eccab3e3a1c2f7082b0671842554
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-05-11 17:27:37 +0530

    ProfileScreen Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,10 +23,12 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+
       {/* Private Routes */}
       <Route path='' element={<PrivateRoute />}>
         <Route path="/profile" element={<ProfileScreen />} />
       </Route>
+      
     </Route>
   )
 );
