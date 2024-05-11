738a571f0307247438cad80d449e48d79e5a7710
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-05-11 16:29:21 +0530

    Create Loader Component

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,32 @@
+import { Spinner } from "react-bootstrap";
+
+const Loader = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Spinner
+        animation="border"
+        role="status"
+        style={{
+          width: "100px",
+          height: "100px",
+          margin: "auto",
+          display: "block",
+        }}
+      ></Spinner>
+    </div>
+  );
+};
+
+export default Loader;
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useLoginMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("");
@@ -36,40 +37,45 @@ const LoginScreen = () => {
   };
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
-
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId="email" className="my-2">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="password" className="my-2">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Button type="submit" variant="primary" className="mt-3">
-          Sign In
-        </Button>
-
-        <Row className="py-3">
-          <Col>
-            New Customer? <Link to="/register">Register</Link>
-          </Col>
-        </Row>
-      </Form>
-    </FormContainer>
+    <>
+    { isLoading ? <Loader /> : 
+       <FormContainer>
+       <h1>Sign In</h1>
+ 
+       <Form onSubmit={submitHandler}>
+         <Form.Group controlId="email" className="my-2">
+           <Form.Label>Email Address</Form.Label>
+           <Form.Control
+             type="email"
+             placeholder="Enter email"
+             value={email}
+             onChange={(e) => setEmail(e.target.value)}
+           ></Form.Control>
+         </Form.Group>
+         <Form.Group controlId="password" className="my-2">
+           <Form.Label>Password</Form.Label>
+           <Form.Control
+             type="password"
+             placeholder="Enter Password"
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+           ></Form.Control>
+         </Form.Group>
+ 
+ 
+         <Button type="submit" variant="primary" className="mt-3">
+           Sign In
+         </Button>
+ 
+         <Row className="py-3">
+           <Col>
+             New Customer? <Link to="/register">Register</Link>
+           </Col>
+         </Row>
+       </Form>
+     </FormContainer>
+    }
+    </>
   );
 };
 
